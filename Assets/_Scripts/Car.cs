d7122d7d37abcb18106c7e0c1d0b1acbe233b137
Remes Anssi TTK17SP <anssiremes@kamk.fi>
2020-02-01 23:40:39 +0200

    Add leathal velocity

--- Assets/_Scripts/Car.cs
+++ Assets/_Scripts/Car.cs
@@ -4,17 +4,32 @@ using UnityEngine;
 
 public class Car : MonoBehaviour
 {
+    private float prevVelocity;
+    private Rigidbody rb = null;
+
+    [SerializeField] private float leathalStop = 5f;
     [SerializeField] private float wheelDriveForce = 1f;
     [SerializeField] private float startingSpeed = 0f;
 
     public float WheelDrivingForce { get => wheelDriveForce; set => wheelDriveForce = value; }
 
-    void Start()
+    private void Start()
     {
+        rb = GetComponent<Rigidbody>();
+
         foreach (Rigidbody rb in GetComponentsInChildren<Rigidbody>())
         {
-            // Right is forward because...
             rb.velocity = transform.right * startingSpeed;
+        }
+
+        prevVelocity = rb.velocity.magnitude;
+    }
+
+    private void FixedUpdate()
+    {
+        if(prevVelocity - rb.velocity.magnitude > leathalStop)
+        {
+            // Die or something
         }
     }
 }
