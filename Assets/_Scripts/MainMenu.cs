5d1c90dd48788bfef9766d23b66b096fe66a4c4c
Sumpp1 <samuelvaananen@outlook.com>
2020-02-01 16:33:36 +0200

    Main Menu

---
+++ Assets/_Scripts/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject credits;
+
+        public void LoadScene(string sceneName)
+        {
+            SceneManager.LoadScene("Jannescene");
+        }
+
+            public void ExitGame()
+            {
+                Application.Quit();
+            }
+
+    public void Credits()
+    {
+        credits.SetActive(true);
+    }
+
+    public void BackToMainMenu()
+    {
+        credits.SetActive(false);
+    }
+        }
+    
+
