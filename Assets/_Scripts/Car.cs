74f9a2e1a03dcfbbfc02568ee742da58fd67d51f
Remes Anssi TTK17SP <anssiremes@kamk.fi>
2020-02-01 14:02:43 +0200

    Add car 1.0

---
+++ Assets/_Scripts/Car.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    [SerializeField] private float wheelDriveForce = 1f;
+    [SerializeField] private float startingSpeed = 0f;
+
+    public float WheelDrivingForce { get => wheelDriveForce; set => wheelDriveForce = value; }
+
+    void Start()
+    {
+        foreach (Rigidbody rb in GetComponentsInChildren<Rigidbody>())
+        {
+            // Right is forward because...
+            rb.velocity = transform.right * startingSpeed;
+        }
+    }
+}
---
+++ Assets/_Scripts/CarWheel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SphereCollider), typeof(Rigidbody), typeof(HingeJoint))]
+public class CarWheel : MonoBehaviour
+{
+    private float driveForce;
+    private float radius;
+    private Rigidbody rb;
+    private Transform car;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        car = transform.parent;
+
+        driveForce = car.GetComponent<Car>().WheelDrivingForce;
+
+        Vector3 extents = GetComponent<Collider>().bounds.extents;
+        radius = Mathf.Max(extents.x, extents.y, extents.z);
+    }
+
+    private void FixedUpdate()
+    {
+        DriveWheel(Time.fixedDeltaTime * driveForce);
+    }
+
+    private void DriveWheel(float amount)
+    {
+        Vector3 force = car.right * amount;
+        Vector3 forcePos = transform.position + car.up * radius;
+
+        rb.AddForceAtPosition(force, forcePos);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawLine(transform.position + car.up * radius, transform.position + Vector3.up * radius + car.right * driveForce);
+    }
+}
