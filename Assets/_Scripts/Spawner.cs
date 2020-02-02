5e0f710be2ad3b446dc2095b19770087bec1a6b4
Sumpp1 <samuelvaananen@outlook.com>
2020-02-02 10:06:10 +0200

    Spawner script

---
+++ Assets/_Scripts/Spawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject car;
+    public float timeLeft;
+    public bool spawned;
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0 && spawned == false)
+        {
+            Instantiate(car, transform.position, transform.rotation);
+            spawned = true;
+        }
+    }
+
+}
